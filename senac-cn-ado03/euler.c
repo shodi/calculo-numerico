dfa1c52809574923bbf447dbecfe5beabb97b168
shodi <hikihenrique@yahoo.com>
2018-04-18 01:18:50 -0300

    error while open pipe

--- senac-cn-ado03/euler.c
+++ senac-cn-ado03/euler.c
@@ -3,21 +3,21 @@
 #include <stdlib.h>
 #include "lib.h"
 
-void open_file_connection(FILE *fp, char *output_name) {
-    fp = popen("gnuplot --persist", "w");
-    fprintf(fp, "set terminal png\n");
-    fprintf(fp, "set output '%s'\n", output_name);
-    fprintf(fp, "set logscale x\n");
-    fprintf(fp, "plot '-' with lines\n");
+void open_file_connection(FILE **fp, char *output_name) {
+    *fp = popen("gnuplot --persist", "w");
+    fprintf(*fp, "set terminal png\n");
+    fprintf(*fp, "set output '%s'\n", output_name);
+    fprintf(*fp, "set logscale x\n");
+    fprintf(*fp, "plot '-' with lines\n");
 }
 
 int main(void) {
     FILE *fp_float, *fp_double, *fp_long_double;
     fp_float = fp_double = fp_long_double = NULL;
     long double iterator = 1;
-    open_file_connection(fp_float, "euler_flt.png");
-    open_file_connection(fp_double, "euler_dbl.png");
-    open_file_connection(fp_long_double, "euler_ldbl.png");
+    open_file_connection(&fp_float, "euler_flt.png");
+    open_file_connection(&fp_double, "euler_dbl.png");
+    open_file_connection(&fp_long_double, "euler_ldbl.png");
     for(; iterator <= _MAX_ITERATION; iterator *= 10) {
         printf("iter_index: %.0Lf\tfloat: %.5f\tdouble: %.5lf\tlong double: %.5Lf\n", iterator, get_euler_float(fp_float, iterator), get_euler_double(fp_double, iterator), get_euler_long_double(fp_long_double, iterator));
     }
